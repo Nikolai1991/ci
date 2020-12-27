4f1a1f79102e67cab9bb9a5aafda2d5056421db1
Nikolai1991 <ndavidov0@gmail.com>
2020-12-27 16:57:31 +0200

    fix: Change app version to 3

--- .releaserc.js
+++ .releaserc.js
@@ -1,5 +1,5 @@
 module.exports = {
-    branch: ["master", "dev"],
+    branch: ["main", "dev"],
     plugins: [
       '@semantic-release/commit-analyzer',
       '@semantic-release/release-notes-generator',
